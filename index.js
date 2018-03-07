f9ac0040ff06e32cf1a8135fe14e2b68651fb7d9
westnorthern <rrichardson610@gmail.com>
2018-03-07 01:01:20 -0700

    added about and workshop pages

---
+++ index.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//   var a = 1;
+//   var add =2;
+
+//   function changeBG(){
+//     var r = Math.floor((255 * a) / 100);  
+//     var g = Math.floor((255 * (100 - a)) / 100);
+//     var b = Math.floor((255 * a) / 100);  
+//     if(a<=0){
+//       add = 2;
+//     }else if(a>99){
+//       add=-2;
+//     }  
+//     a=a+add;    
+//     $('.splash').css('background-color', 'rgb('+r+','+g+','+b+')');
+//   }    
+//   setInterval(changeBG, 1000);
+
+// });
